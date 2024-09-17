477f191608d2fc7f61a6ead96942cb868422d4b0
vonhatphuongahihi <vonhatphuongahihi@gmail.com>
2024-09-17 21:43:10 +0700

    check_booking

--- index.php
+++ index.php
@@ -33,6 +33,24 @@
         input[type=number] {
             -moz-appearance: textfield;
         }
+        .custom-bg{
+            background-color: #2EC1AC;
+        }
+        .custom-bg:hover{
+            background-color: #279E8C;
+        }
+        .availability-form{
+            margin-top: -50px;
+            z-index: 2;
+            position: relative;
+        }
+        @media screen and (max-width: 575px){
+            .availability-form{
+                margin-top: 25px;
+                padding:0 35px;
+            }
+            
+        }
     </style>
 </head>
 <body class="bg-light">
@@ -167,6 +185,7 @@
             </div>
         </div>
     </div>
+    <!-- Carousel -->
     <div class="container-fluid px-lg-4 mt-4">
         <div class="swiper swiper-container">
         <div class="swiper-wrapper">
@@ -191,6 +210,50 @@
         </div>
     </div>
     </div>
+    <!-- Check booking availability form -->
+    <div class="container availability-form">
+        <div class="row">
+            <div class="col-lg-12 bg-white shadow p-4 rounded">
+                <h5 class="mb-4">Check Booking Availability</h5>
+                <form>
+                    <div class="row align-items-end">
+                        <div class="col-lg-3 mb-3">
+                            <label class="form-label"
+                            style="font-weight: 500;">Check-in</label>
+                            <input type="date" class="form-control shadow-none">
+                        </div>
+                        <div class="col-lg-3 mb-3">
+                            <label class="form-label"
+                            style="font-weight: 500;">Check-out</label>
+                            <input type="date" class="form-control shadow-none">
+                        </div>
+                        <div class="col-lg-3 mb-3">
+                            <label class="form-label"
+                            style="font-weight: 500;">Adult</label>
+                            <select class="form-select shadow-none">
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                        <div class="col-lg-2 mb-3">
+                            <label class="form-label"
+                            style="font-weight: 500;">Children</label>
+                            <select class="form-select shadow-none">
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                        <div class="col-lg-1 mb-lg-3 mt-2">
+                            <button type="submit" class="btn text-white shadow-none custom-bg">Submit</button>
+                        </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <br><br><br>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script>
