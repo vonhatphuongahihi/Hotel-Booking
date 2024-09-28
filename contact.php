d76fd27a86424c92966c1b0d6b934efcfb52fa71
hoangminh1252010 <hoangminh3008200404@gmail.com>
2024-09-29 01:01:20 +0700

    update_minh

--- contact.php
+++ contact.php
@@ -27,40 +27,58 @@
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione dolorem <br> eligendi nulla qui fugiat fuga neque provident. Voluptas, quod quia!
     </p>
     </div>
+    
     <div class="container">
         <div class="row">
             <div class="col-lg-4 col-md-6 mb-5 px-4">
                 <div class="bg-white rounded shadow p-4">
-                    <iframe class="w-100 rounded mb-4" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d249525.41415596197!2d109.24650199999999!3d12.25947015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3170677811cc886f%3A0x5c4bbc0aa81edcb9!2zTmhhIFRyYW5nLCBLaMOhbmggSMOyYSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1726658262133!5m2!1svi!2s" loading="lazy"></iframe>
+                    <iframe class="w-100 rounded mb-4" height="320px" src="<?php echo $contact_r['iframe'] ?> " loading="lazy"></iframe>
                         <h5>Address</h5>
-                        <a href="https://maps.app.goo.gl/5krry61WQzK7eX5W9 " target="_blank" class="d-inline-block text-decoration-none text-dark mb-2">
-                        <i class="bi bi-geo-alt"></i>  Muong Thanh Hotel 
+                        <a href=" <?php echo $contact_r['gmap'] ?>" target="_blank" class="d-inline-block text-decoration-none text-dark mb-2">
+                        <i class="bi bi-geo-alt"></i> <?php echo $contact_r['address'] ?>
                         </a>
                         <h5 class="mt-4">Call us</h5>
-                    <a href="tel: +84777888999" class="d-inline-block mb-2 text-decoration-none text-dark">
-                        <i class="bi bi-telephone-fill"></i> +84777888999
+                    <a href="tel: +<?php echo $contact_r['pn1'] ?>" class="d-inline-block mb-2 text-decoration-none text-dark">
+                        <i class="bi bi-telephone-fill"></i> +<?php echo $contact_r['pn1'] ?>
                     </a>
                     <br>
-                    <a href="tel: +84777888999" class="d-inline-block text-decoration-none text-dark">
-                        <i class="bi bi-telephone-fill"></i> +84777888999
-                    </a>
+                    <?php
+                    if($contact_r['pn2']!='')
+                    {
+                        echo<<<data
+                            <a href="tel: +$contact_r[pn2]" class="d-inline-block text-decoration-none text-dark">
+                            <i class="bi bi-telephone-fill"></i> +$contact_r[pn2]
+                            </a>
+                        data;
+                    } 
+                    ?>
+                
                     <h5 class= "mt-4">
                         Email
                     </h5>
-                    <a href="mailto : hoangminh3008200404@gmail.com" class=" d-inline-block text-decoration-none text-dark">
+                    <a href="mailto : <?php echo $contact_r['email'] ?>" class=" d-inline-block text-decoration-none text-dark">
                     <i class="bi bi-envelope-fill"></i>
-                    hoangminh3008200404@gmail.com
+                    <?php echo $contact_r['email'] ?>
                     </a>
                     <h5 class="mt-4">Follow us</h5>
-                    <a href="#" class="d-inline-block text-dark fs-5 me-2">
+                    <?php
+                    if($contact_r['tw']!='')
+                    {
+                        echo<<<data
+                            <a href="$contact_r[tw]" class="d-inline-block text-dark fs-5 me-2">
                        
-                        <i class="bi bi-twitter-x me-1"></i>
+                             <i class="bi bi-twitter-x me-1"></i>
                         
-                    </a>
-                    <a href="#" class="d-inline-block text-dark fs-5 me-2">
+                            </a>
+
+                        data;
+                    }
+                    ?>
+            
+                    <a href="<?php echo $contact_r['fb'] ?>" class="d-inline-block text-dark fs-5 me-2">
                         <i class="bi bi-facebook me-1"></i>
                     </a>
-                    <a href="#" class="d-inline-block text-dark fs-5">
+                    <a href="<?php echo $contact_r['insta'] ?>" class="d-inline-block text-dark fs-5">
 
                         <i class="bi bi-instagram me-1"></i>
                     </a>
--- index.php
+++ index.php
@@ -39,24 +39,19 @@
     <div class="container-fluid px-lg-4 mt-4">
         <div class="swiper swiper-container">
         <div class="swiper-wrapper">
-        <div class="swiper-slide">
-            <img src="images/carousel/1.png" class="w-100 d-block" />
-        </div>
-        <div class="swiper-slide">
-            <img src="./images/carousel/2.png" class="w-100 d-block"/>
-        </div>
-        <div class="swiper-slide">
-            <img src="images/carousel/3.png" class="w-100 d-block"/>
-        </div>
-        <div class="swiper-slide">
-            <img src="images/carousel/4.png" class="w-100 d-block" />
-        </div>
-        <div class="swiper-slide">
-            <img src="images/carousel/5.png" class="w-100 d-block"/>
-        </div>
-        <div class="swiper-slide">
-            <img src="images/carousel/6.png" class="w-100 d-block"/>
-        </div>
+            <?php
+                   $res = selectAll('carousel');
+                   while ($row = mysqli_fetch_assoc($res)) {
+                    $path = CAROUSEL_IMG_PATH;
+                    echo <<<data
+                    $path$row[image]
+                         <div class="swiper-slide">
+                           <img src="$path$row[image]" class="w-100 d-block" />
+                         </div>
+                  
+                    data;
+                 }
+            ?>    
         </div>
     </div>
     </div>
@@ -471,39 +466,57 @@
         </div>
     </div>
     <!-- Reach us -->
+    
+    
     <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">REACH US</h2>
 
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
-                <iframe class="w-100 rounded" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d249525.41415596197!2d109.24650199999999!3d12.25947015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3170677811cc886f%3A0x5c4bbc0aa81edcb9!2zTmhhIFRyYW5nLCBLaMOhbmggSMOyYSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1726658262133!5m2!1svi!2s" loading="lazy"></iframe>
+                <iframe class="w-100 rounded" height="320px" src="<?php echo $contact_r['iframe']?>" loading="lazy"></iframe>
             </div>
             <div class="col-lg-4 col-md-4">
                 <div class="bg-white p-4 rounded mb-4">
                     <h5>Call us</h5>
-                    <a href="tel: +84 365 486 141" class="d-inline-block mb-2 text-decoration-none text-dark">
-                        <i class="bi bi-telephone-fill"></i> +84 365 486 141
+                    <a href="tel: +<?php echo $contact_r['pn1']?>" class="d-inline-block mb-2 text-decoration-none text-dark">
+                        <i class="bi bi-telephone-fill"></i> +<?php echo $contact_r['pn1']?>
                     </a>
                     <br>
-                    <a href="tel: +84 365 486 141" class="d-inline-block text-decoration-none text-dark">
-                        <i class="bi bi-telephone-fill"></i> +84 365 486 141
-                    </a>
+                    <?php
+                    if($contact_r['pn2']!='' )
+                    {
+                        echo<<<data
+                         <a href="tel: +$contact_r[pn2]" class="d-inline-block text-decoration-none text-dark">
+                        <i class="bi bi-telephone-fill"></i> +$contact_r[pn2]
+                         </a>    
+                        data;
+                    }
+                    ?>
+                   
                 </div>
                 <div class="bg-white p-4 rounded mb-4">
                     <h5>Follow us</h5>
-                    <a href="https://x.com/i/flow/single_sign_on" class="d-inline-block mb-3">
-                        <span class="badge bg-light text-dark fs-6 p-2">
-                        <i class="bi bi-twitter-x me-1"></i> X (Twitter)
-                        </span>
-                    </a>
-                    <br>
-                    <a href="https://www.facebook.com/phuong.vonhat.tuhy/" class="d-inline-block mb-3">
+                    <?php
+                    if($contact_r['tw']!= '')
+                    {
+                        echo<<<data
+                         <a href="$contact_r[tw]" class="d-inline-block mb-3">
+                          <span class="badge bg-light text-dark fs-6 p-2">
+                          <i class="bi bi-twitter-x me-1"></i> (Twitter)
+                          </span>
+                         </a>
+                         <br>
+                        data;
+                    }
+                    ?>
+            
+                    <a href="<?php echo $contact_r['fb'] ?>" class="d-inline-block mb-3">
                         <span class="badge bg-light text-dark fs-6 p-2">
                         <i class="bi bi-facebook me-1"></i> Facebook
                         </span>
                     </a>
                     <br>
-                    <a href="https://www.instagram.com/tuhy.sapoche.99/" class="d-inline-block">
+                    <a href="<?php echo $contact_r['insta'] ?>" class="d-inline-block">
                         <span class="badge bg-light text-dark fs-6 p-2">
                         <i class="bi bi-instagram me-1"></i> Instagram
                         </span>
