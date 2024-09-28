7504bc847214c43898bf39f9cef2ac2840dc0a37
vonhatphuongahihi <vonhatphuongahihi@gmail.com>
2024-09-28 12:05:00 +0700

    contact_management_team

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -18,6 +18,13 @@
        }
        return $data;
     }
+
+    function selectAll($table){
+        $con = $GLOBALS['con'];
+        $res = mysqli_query($con, "SELECT * FROM $table");
+        return $res;
+    }
+
     function select($sql, $values, $datatypes) {
         $con = $GLOBALS['con'];
         if ($stmt = mysqli_prepare($con, $sql)) {
@@ -60,4 +67,45 @@
         }
     }
 
+    function insert($sql, $values, $datatypes) {
+        $con = $GLOBALS['con'];
+        if ($stmt = mysqli_prepare($con, $sql)) {
+            mysqli_stmt_bind_param($stmt, $datatypes, ...$values);
+            if (mysqli_stmt_execute($stmt))
+            {
+                $result = mysqli_stmt_affected_rows($stmt);
+                mysqli_stmt_close($stmt);
+                return $result;
+            }
+            else {
+                die("Query cannot be executed - Insert");
+            }
+            mysqli_stmt_close($stmt);
+            
+        }
+        else{
+            die("Query cannot be prepared - Insert");
+        }
+    }
+
+    function delete($sql, $values, $datatypes) {
+        $con = $GLOBALS['con'];
+        if ($stmt = mysqli_prepare($con, $sql)) {
+            mysqli_stmt_bind_param($stmt, $datatypes, ...$values);
+            if (mysqli_stmt_execute($stmt))
+            {
+                $result = mysqli_stmt_affected_rows($stmt);
+                mysqli_stmt_close($stmt);
+                return $result;
+            }
+            else {
+                die("Query cannot be executed - Delete");
+            }
+            mysqli_stmt_close($stmt);
+            
+        }
+        else{
+            die("Query cannot be prepared - Delete");
+        }
+    }
 ?>
